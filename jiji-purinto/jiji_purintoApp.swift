a3e5f1a2b3ce9f39543d7ada3e24dd2d14e49ba9
Denis Moiseev <lobziiko.o@gmail.com>
2026-01-20 20:27:50 +0100

    Initial Commit

---
+++ jiji-purinto/jiji_purintoApp.swift
@@ -0,0 +1,17 @@
+//
+//  jiji_purintoApp.swift
+//  jiji-purinto
+//
+//  Created by lobziik on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct jiji_purintoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jiji-purintoUITests/jiji_purintoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  jiji_purintoUITestsLaunchTests.swift
+//  jiji-purintoUITests
+//
+//  Created by lobziik on 20.01.2026.
+//
+
+import XCTest
+
+final class jiji_purintoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
